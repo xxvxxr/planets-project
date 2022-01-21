200d9a2f582dd42d1e22d5eaf19b29a53a226f94
Xavier Bradley <xvrbradley@gmail.com>
2022-01-20 21:09:21 -0700

    8 habitable planets found!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const habitablePlanets = [];
 
-function isHabitablePlanet(planet) {
+const isHabitablePlanet = planet => {
   return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
@@ -14,16 +14,16 @@ fs.createReadStream('kepler_data.csv')
     comment: '#',
     columns: true,
   }))
-  .on('data', (data) => {
+  .on('data', data => {
     if (isHabitablePlanet(data)) {
       habitablePlanets.push(data);
     }
   })
   .on('error', (err) => {
-    console.log(err);
+    console.error(err);
   })
   .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
+    console.log(habitablePlanets.map(planet => {
       return planet['kepler_name'];
     }));
     console.log(`${habitablePlanets.length} habitable planets found!`);
